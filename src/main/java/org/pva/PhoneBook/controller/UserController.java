d8878a3beea6f6e1b9aad52c0c2930a43216313a
viktorvx <victorptrv@yandex.ru>
2019-06-13 23:08:47 +0300

    Add ability to save user info to database

--- src/main/java/org/pva/PhoneBook/controller/UserController.java
+++ src/main/java/org/pva/PhoneBook/controller/UserController.java
@@ -6,11 +6,13 @@ import org.pva.PhoneBook.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
@@ -32,8 +34,30 @@ public class UserController {
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
-
         return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user
+    ) {
+        user.setUsername(username);
+        System.out.println(username);
+        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+        System.out.println(roles);
+        System.out.println(form.keySet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        System.out.println(user.getRoles());
+        userRepo.save(user);
+
+        return "redirect:/user";
     }
 
 
