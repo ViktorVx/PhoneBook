abaf220278ab53c2f6fa38fc115e12f40f78b0a3
viktorvx <victorptrv@yandex.ru>
2019-06-13 23:14:20 +0300

    Only admin can give roles to users

--- src/main/java/org/pva/PhoneBook/controller/UserController.java
+++ src/main/java/org/pva/PhoneBook/controller/UserController.java
@@ -4,6 +4,7 @@ import org.pva.PhoneBook.domain.Role;
 import org.pva.PhoneBook.domain.User;
 import org.pva.PhoneBook.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     @Autowired
