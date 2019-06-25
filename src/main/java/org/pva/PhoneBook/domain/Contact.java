c64e5f6902a67e719cee3a2182303437cffd729f
viktorvx <victorptrv@yandex.ru>
2019-06-25 21:28:13 +0300

    Add graphml-scheme (uml)

--- src/main/java/org/pva/PhoneBook/domain/Contact.java
+++ src/main/java/org/pva/PhoneBook/domain/Contact.java
@@ -44,11 +44,11 @@ public class Contact {
         this.owner = owner;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
